8e57ef9c460ba742f157bae284276b1184b72d7f
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-11-26 00:34:00 +0300

    add functional for tariff plans

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+const pricingButtons = document.querySelectorAll(".pricing__btn");
+const pricingCards = document.querySelectorAll(".pricing__price");
+
+function updatePrices(period) {
+  pricingCards.forEach((card) => {
+    const totalElement = card.querySelector(".pricing__total");
+    const basePrice = parseFloat(totalElement.dataset.basePrice);
+    let newPrice;
+    let timeUnit;
+
+    if (period === "Monthly") {
+      newPrice = basePrice;
+      timeUnit = "/month";
+    } else if (period === "Yearly") {
+      newPrice = (basePrice * 12 * 0.5).toFixed(0);
+      timeUnit = "/year";
+    } else if (period === "Half a Year") {
+      newPrice = (basePrice * 12 * 0.25).toFixed(0);
+      timeUnit = "/half a year"
+    }
+
+    totalElement.textContent = `$${newPrice}`;
+    card.querySelector(".pricing__date").textContent = timeUnit;
+  })
+}
+
+pricingButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    pricingButtons.forEach((btn) => btn.classList.remove("pricing__btn--active"));
+    button.classList.add("pricing__btn--active");
+    const selectedPeriod = button.textContent.trim();
+    updatePrices(selectedPeriod);
+  });
+});
+
+/*
 const people = [
   {
     name: 'Павел Бойко', age: 66, fit: true, education: "secondary"
@@ -27,4 +63,4 @@ const existsEligible = people.some(isEligibleForConscription); // квантор
 console.log("Есть ли хотя бы один призывник? " + existsEligible);
 
 const uniqueEligible = people.filter(isEligibleForConscription).length === 1; // квантор единственности
-console.log("Есть ли ровно один призывник? " + uniqueEligible);
+console.log("Есть ли ровно один призывник? " + uniqueEligible);*/
