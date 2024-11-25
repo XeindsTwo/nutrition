f43acd397e77662073589f6f57a9af5e50102f59
ArkadiyGenij <agongadze8@gmail.com>
2024-11-25 11:51:31 +0300

    fixes pricing section

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const people = [
   {
-    name: 'Павел Бойко', age: 20, fit: true, education: "Среднее"
+    name: 'Павел Бойко', age: 66, fit: true, education: "secondary"
   },
   {
-    name: 'Роберт Калайджан', age: 27, fit: true, education: "Высшее"
+    name: 'Роберт Калайджан', age: 20, fit: true, education: "primary"
   },
   {
-    name: 'Варосян Алекс', age: 43, fit: true, education: "Среднее"
+    name: 'Варосян Алекс', age: 43, fit: true, education: "higher"
   },
   {
-    name: 'Адонис Мантаджиев', age: 15, fit: true, education: "начальное"
-  }
+    name: 'Адонис Мантарджиев', age: 66, fit: true, education: "primary"
+  },
 ];
 
 function isEligibleForConscription(person) {
@@ -19,3 +19,12 @@ function isEligibleForConscription(person) {
 
 const eligibleForService = people.filter(isEligibleForConscription);
 console.log(eligibleForService);
+
+const allEligible = people.every(isEligibleForConscription); // квантор всеобщности
+console.log("Все люди являются призывниками? " + allEligible);
+
+const existsEligible = people.some(isEligibleForConscription); // квантор существования
+console.log("Есть ли хотя бы один призывник? " + existsEligible);
+
+const uniqueEligible = people.filter(isEligibleForConscription).length === 1; // квантор единственности
+console.log("Есть ли ровно один призывник? " + uniqueEligible);
